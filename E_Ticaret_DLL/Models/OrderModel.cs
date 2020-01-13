b0448fd3648d5005697b2432134f422ee6f8e0de
Sadullah <sadullah.dogan@yandex.com>
2020-01-13 13:24:32 +0300

    Update

---
+++ E_Ticaret_DLL/Models/OrderModel.cs
@@ -0,0 +1,19 @@
+﻿using E_Ticaret_Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticaret_DLL.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public double Total { get; set; }
+        public EnumOrderState OrderState { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ E_Ticaret_WEBUI/Controllers/OrderController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using E_Ticaret_DAL.DB;
+using E_Ticaret_Entity.Entity;
+
+namespace E_Ticaret_WEBUI.Controllers
+{
+    [Authorize(Roles ="admin")]
+    public class OrderController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        public ActionResult ConfirmOrder(int id) {
+            
+            var order = db.Orders.FirstOrDefault(x => x.Id == id);
+            order.OrderState = EnumOrderState.Completed;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Order
+        public ActionResult Index()
+        {
+            
+            return View(db.Orders.ToList());
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,OrderNumber,Total,OrderDate,OrderState,Username,AdresBilgi,Adres,Sehir,Semt,Mahalle,PostaKodu")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,OrderNumber,Total,OrderDate,OrderState,Username,AdresBilgi,Adres,Sehir,Semt,Mahalle,PostaKodu")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
